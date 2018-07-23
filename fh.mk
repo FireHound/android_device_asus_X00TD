f69e66c4bfcdbc283a264c89cbe70e34aad1f53c
akshatsinha2002 <sinhaakshat2012@gmail.com>
2018-07-23 17:53:15 +0000

    X00TD: Compile with clang 7.0.2
    Change-Id: If8927498bc440c75a920b526e70d0107ff1e69ef

--- fh.mk
+++ fh.mk
@@ -39,3 +39,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="sdm660_64-user 8.1.0 OPM1 13
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := Android/sdm660_64/sdm660_64:8.1.0/OPM1/15.2016.1805.318-20180712:user/release-keys
+
+FH_WEEKLY=true
